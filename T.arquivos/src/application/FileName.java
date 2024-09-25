2e243c9f1083e61c14a5eb41b8c10dd7db744123
Ismael Alves <ismael.a.eugenio@gmail.com>
2024-09-25 14:58:47 -0300

    Finished

--- T.arquivos/src/application/FileName.java
+++ T.arquivos/src/application/FileName.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 public class FileName {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter a file path: ");
-		String strPath = sc.nextLine();
-		
-		File path = new File(strPath);
-		
-		System.out.println("getName: " + path.getName());
-		System.out.println("getParent: " + path.getParent());
-		System.out.println("getPath: " + path.getPath());
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter a file path: ");
+			String strPath = sc.nextLine();
+			
+			File path = new File(strPath);
+			
+			System.out.println("getName: " + path.getName());
+			System.out.println("getParent: " + path.getParent());
+			System.out.println("getPath: " + path.getPath());
+		}
 		
 		
 
